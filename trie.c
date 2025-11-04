97457d4a661e68ad80637ddbcd165652815062b5
ezk1d <depdhar8@gmail.com>
2025-11-04 16:30:52 -0600

    Complete

--- trie.c
+++ trie.c
@@ -10,14 +10,14 @@ static void free_trie_node(trie_node_t** node);
  * @param prefix the prefix to search with
  */
 void autocomplete(trie_node_t* root, char* prefix) {
-    if (!prefix) {
+    if (!prefix || !root) {
         return;
     }
     printf("finding all completions of prefix \"%s\":\n", prefix);
     int len = 0;
     trie_node_t* letter = root;
     char* itr = prefix;
-    while (*itr && len < MAX_WORD_LENGTH) {
+    while (*itr && len < MAX_WORD_LENGTH && letter)  {
         letter = letter->children[*itr - FIRST_VALID_CHAR];
         itr++;
         len++;
